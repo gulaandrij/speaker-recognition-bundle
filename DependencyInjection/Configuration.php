c9f58ed5a3bfc5fa5809fe6ab9a5243e4731760b
gylaandrij <gyla.andrij@gmail.com>
2018-10-23 12:59:51 +0300

    fix

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace Onekit\SpeakerRecognitionBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+
 class Configuration implements ConfigurationInterface
 {
     /**
--- DependencyInjection/OnekitSpeakerRecognitionExtension.php
+++ DependencyInjection/OnekitSpeakerRecognitionExtension.php
@@ -3,8 +3,8 @@
 namespace Onekit\SpeakerRecognitionBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class OnekitSpeakerRecognitionExtension extends Extension
